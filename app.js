8ad062ac18aa6da589ecef1f1f307a8f388f8e21
A007-cloud <dasaditya788@gmail.com>
2021-09-02 20:58:07 +0530

    init

--- app.js
+++ app.js
@@ -12,11 +12,22 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();  
     }
-    if(url == '/message' && method == 'POST') {
-        fs.writeFileSync('message.txt', 'DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+    if(url === '/message' && method ==='POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            // console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.text', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();  
+            });
+        });  
     }
     // process.exit();
     res.setHeader('Content-Type', 'text/html');
